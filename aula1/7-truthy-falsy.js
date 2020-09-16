b3fdb172dc7258b42d88af383e51c1f017471c6f
Moacir Costa <moacircostajr@gmail.com>
2020-09-16 09:22:01 -0300

    alteracoes

--- aula1/7-truthy-falsy.js
+++ aula1/7-truthy-falsy.js
@@ -6,12 +6,12 @@
 // null
 // undefined
 // NaN
-// Everything else is truthy. That includes:
 
+// Everything else is truthy. That includes:
 // '0' (a string containing a single zero)
 // 'false' (a string containing the text “false”)
 // [] (an empty array)
 // {} (an empty object)
 // function(){} (an “empty” function)
 
-console.log(!!0);
+console.log(!!function () {});
