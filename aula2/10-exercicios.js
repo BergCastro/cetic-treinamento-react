3dc711525b54b9d4b62f76b54306271ad7eb29fe
Jordão <jordaomemoria@gmail.com>
2020-09-16 10:04:38 -0300

    Exercício 10

--- aula2/10-exercicios.js
+++ aula2/10-exercicios.js
@@ -2,7 +2,31 @@
 // -> Filtrar pares
 // -> Dobrar os valores
 // -> Reduzir (somar tudo)
-const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
+const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27]
 
 //1 - fazer com três constantes
+const pares = numeros.filter(element => {
+    return element%2 === 0
+})
+console.log('Pares: ',pares)
+
+const dobra = numeros.map(element => {
+    return element*2
+})
+console.log('Dobrar: ', dobra)
+
+const acumulador = numeros.reduce((n1, n2) => {
+    return n1+n2
+})
+console.log('Acumulador: ', acumulador)
+
+
 // 2 - fazer de forma encadeada
+const resposta = numeros.filter(element => {
+    return element%2 === 0
+    }).map(element => {
+        return element*2
+    }).reduce((n1, n2) => {
+        return n1+n2
+    })
+console.log('Encadeado: ', resposta)
