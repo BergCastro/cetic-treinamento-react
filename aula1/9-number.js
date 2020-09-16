2932562eafc9928b75ea58386ad3efa5ddea6701
Lindemberg <lindembergncastro@outlook.com>
2020-09-16 09:18:50 -0300

    aula2 completa

--- aula1/9-number.js
+++ aula1/9-number.js
@@ -1,4 +1,3 @@
-// IEEE 754-2008
 let num1 = 0.7; // number
 let num2 = 0.1; // number
 
@@ -10,7 +9,6 @@ num1 = Number(num1.toFixed(2));
 
 console.log(num1.toFixed(2));
 console.log(Number.isInteger(num1));
-
 
 // console.log(num1.toString() + num2);
 // num1 = num1.toString();
