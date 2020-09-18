305073ea8ae71b4900dda8bde0a16bdbbb59bd7e
Moacir Costa <moacircostajr@gmail.com>
2020-09-18 09:39:45 -0300

    incluidas constantes

--- aula2/10-exercicios.js
+++ aula2/10-exercicios.js
@@ -4,11 +4,16 @@
 // -> Reduzir (somar tudo) (reduce)
 const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
 
-//1 - fazer com três constantes
-// 2 - fazer de forma encadeada
-
 const resultado = numeros
   .filter((valor) => valor % 2 === 0)
   .map((valor) => valor * 2)
   .reduce((acumulador, valor) => (acumulador += valor), 0);
 console.log(resultado);
+
+//1 - fazer com três constantes
+// 2 - fazer de forma encadeada
+
+const resultadoFiltragem = numeros.filter((valor) => valor % 2 === 0);
+const resultadoDobra = resultadoFiltragem.map((valor) => valor * 2);
+const resultadoAcumulacao = resultadoDobra.reduce((acumulador, valor) => (acumulador += valor), 0);
+console.log(resultadoAcumulacao);
