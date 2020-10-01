d172122c6784a65786f5336431b6941c913cc933
Lindemberg <lindembergncastro@outlook.com>
2020-10-01 11:50:45 -0300

    axios e useEffect

--- Projeto/src/pages/Home/index.jsx
+++ Projeto/src/pages/Home/index.jsx
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Box from "../../components/Box";
 import PostItem from "../../components/PostItem";
 import RoundButton from "../../components/RoundButton";
 import InputSearch from "../../components/InputSearch";
-
+import api from "../../services/api";
 import { Container, Posts, Filters } from "./styles";
 
 const Home = () => {
+  const [posts, setPosts] = useState([]);
+
+  const categories = ["HTML", "Jabascript", "CSS", "React"];
+
+  useEffect(() => {
+    async function loadPosts() {
+      const { data } = await api.get("/posts");
+
+      setPosts(data);
+    }
+
+    loadPosts();
+  }, []);
+
   return (
     <>
       <Container>
@@ -14,35 +28,14 @@ const Home = () => {
           <h2>Posts</h2>
           <hr />
 
-          <PostItem
-            title="Post 1"
-            author="Lindemberg"
-            datePost="23 de setembro de 2020"
-          />
-
-          <PostItem
-            title="Post 2"
-            author="Lindemberg"
-            datePost="23 de setembro de 2020"
-          />
-
-          <PostItem
-            title="Post 3"
-            author="Lindemberg"
-            datePost="23 de setembro de 2020"
-          />
-
-          <PostItem
-            title="Post 4"
-            author="Lindemberg"
-            datePost="23 de setembro de 2020"
-          />
-
-          <PostItem
-            title="Post 5"
-            author="Lindemberg"
-            datePost="23 de setembro de 2020"
-          />
+          {posts.map((post, index) => (
+            <PostItem
+              key={index}
+              title={post.title}
+              author={post.author}
+              datePost={post.datePost}
+            />
+          ))}
         </Posts>
 
         <Filters>
@@ -53,18 +46,11 @@ const Home = () => {
           <Box title="Categorias">
             <div>
               <ul>
-                <li>
-                  <a href="#">HTML</a>
-                </li>
-                <li>
-                  <a href="#">Javascript</a>
-                </li>
-                <li>
-                  <a href="#">CSS</a>
-                </li>
-                <li>
-                  <a href="#">React</a>
-                </li>
+                {categories.map((category, index) => (
+                  <li key={index}>
+                    <a href="">{category}</a>
+                  </li>
+                ))}
               </ul>
             </div>
           </Box>
